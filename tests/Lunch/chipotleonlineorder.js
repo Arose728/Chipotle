ea216acb2299ef27aec2cd43985d911a7530fb74
Arose728 <arose728@gmail.com>
2017-01-26 16:51:09 -0500

    update order

--- tests/Lunch/chipotleonlineorder.js
+++ tests/Lunch/chipotleonlineorder.js
@@ -7,6 +7,9 @@ module.exports = {
     client
       .url('https://chipotle.com/')
       //.maximizeWindow()
+      .url(function(result){
+        console.log(result);
+      })
       .waitForElementPresent('body', 2000)
       .saveScreenshot('./screenshots/screenshot1.png')
       .useXpath()
@@ -45,16 +48,62 @@ module.exports = {
       //.assert.containsText("#main", "Clifton")
       //.assert.urlEquals('https://order.chipotle.com/?_ga=1.48495968.795510196.1485274896')
       .useCss()
-      .end();
+
   },
-/*
+
   'order' : function(client) {
     client
-      .
-      .click('#search-button')
-      .pause(5000)
-    
-  }
-  */
+      .useXpath()
+      //.click('#Burrito bowl')
+      //.click('button[name=burrio bowl]')
+      .click('/html/body/div/div/cmg-order-entree/div/div[3]/cmg-entrees/div/div[1]/div[2]/div/a/cmg-entree-tile')
+      //.waitForElementPresent('body', 3000)
+      .pause(2000)
+            .url(function(result){
+        console.log("Meat?")
+      })
+      .click('//*[@id="carnitas"]')
+      //.pause(2000)
+      .url(function(result){
+        console.log("Rice?")
+      })
+      .click('//*[@id="ricebrown"]')
+      .url(function(result){
+        console.log("Beans?")
+      })
+      .click('//*[@id="beansblack"]')
+      .url(function(result){
+        console.log("Want guac?")
+      })
+      .click('//*[@id="guactopping"]')
+      .url(function(result){
+        console.log("I know it costs extra!")
+        console.log("Extras?")
+      })
+      .click('//*[@id="salsacorn"]')
 
+      .click('//*[@id="salsagreenchili"]')
+
+      .click('//*[@id="cheese"]')
+
+      .click('//*[@id="lettuce"]')
+
+      .click('//*[@id="chipsandguacamole"]')
+
+      .click('//*[@id="sodalarge"]')
+      .url(function(result){
+        console.log("add to bag")
+      })
+      .click('//*[@id="next"]')
+  },
+
+  'special instructions': function(client) {
+    client
+      .click('//*[@id="bag-container"]')
+      //.click('//*[@id="bag-container"]/div[4]/div[2]/div[2]/div/div/cmg-meal-summary/div/div[1]/div[1]/a')
+      .useCss()
+      .setValue('input[type=text]', ['Alex Rose'])
+
+  //.end();
+  }
 };
